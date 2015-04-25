8a076440504b25bcf02b336f9fe2fadab4f93c31
Alexander Kratzsch <alex@devrandom.se>
2015-04-25 20:19:11 +0200

    added footer, and fixed layout for reports

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,16 @@
 module ReportsHelper
+  def color_status status
+    type = nil
+
+    case status
+    when 'pass'
+      type = 'success'
+    when 'fail'
+      type = 'danger'
+    when 'running'
+      type = 'info'
+    end
+
+    "<span class=\"label label-#{type}\">#{status.capitalize}</span>".html_safe
+  end
 end
