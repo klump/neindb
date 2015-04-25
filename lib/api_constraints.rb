40776f992c4f84f7db938ef5b535e00f39f8f4b2
Alexander Kratzsch <alex@devrandom.se>
2015-04-25 14:48:03 +0200

    added test for api_constraints

--- lib/api_constraints.rb
+++ lib/api_constraints.rb
@@ -1,10 +1,14 @@
 class ApiConstraints
+  # Create a new version of the API
+  # Parameter: A Hash containing the following keys
+  #   :version    Integer     The version of the API
+  #   :default    Boolean     Should this version of the API be default?
   def initialize(options)
     @version = options[:version]
     @default = options[:default]
   end
 
-  def matches?(req)
-    @default || req.headers['X-Nein-API-Version'].include?(@version)
+  def matches?(request)
+    @default || request.headers['X-Nein-API-Version'].to_s == @version.to_s
   end
 end
