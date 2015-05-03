60011e3e099381dd7ca691d81b3fadd84c3b2618
Alexander Kratzsch <alex@devrandom.se>
2015-05-03 10:39:25 +0200

    include changes to an assets component in revisions

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -2,6 +2,8 @@ require 'active_record/diff'
 
 class Asset < ActiveRecord::Base
   include ActiveRecord::Diff
+  
+  diff include: [:serialized_components]
 
   has_many :statuses
   has_many :revisions, as: :revisionable
@@ -10,4 +12,8 @@ class Asset < ActiveRecord::Base
 
   validates :name, presence: true, uniqueness: {scope: [:type]}
   validates :type, presence: true
+
+  def serialized_components
+    components.map { |c| c.serializable_hash }
+  end
 end
