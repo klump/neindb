ab426baf8ad985ffb511ded2c7f0447eb8dcd90c
Alexander Kratzsch <alex@devrandom.se>
2015-05-03 02:26:45 +0200

    display the changes in a human friendly way

--- app/helpers/revisions_helper.rb
+++ app/helpers/revisions_helper.rb
@@ -1,2 +1,19 @@
 module RevisionsHelper
+ def extract_data data, version
+   position = ( version == :old ? 0 : 1 )
+   filtered = {}
+   data.each do |attr,changes|
+     filtered[attr] = changes[position]
+   end
+
+   filtered
+ end
+
+ def print_old_data data
+   "<pre class=\"bg-danger\">#{extract_data(data, :old).to_yaml}</pre>".html_safe
+ end
+
+ def print_new_data data
+   "<pre class=\"bg-success\">#{extract_data(data, :new).to_yaml}</pre>".html_safe
+ end
 end
