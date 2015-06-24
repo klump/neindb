4626c007abcfd6a84bbbf0fab449f52f61ffe593
Alexander Kratzsch <alex@devrandom.se>
2015-06-24 11:58:44 +0200

    make location optional

--- app/models/asset/computer.rb
+++ app/models/asset/computer.rb
@@ -4,7 +4,6 @@ class Asset::Computer < Asset
   validates :product_name, presence: true
   validates :bios_version, presence: true
   validates :bios_vendor, presence: true
-  validates :location, presence: true
   validates :pci_slots, presence: true, numericality: {only_integer: true}
   validates :pcie_slots, presence: true, numericality: {only_integer: true}
   validates :dimm_slots, presence: true, numericality: {only_integer: true}
