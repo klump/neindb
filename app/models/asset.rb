0ebfb51e9f95cf013d420dde4ef748505a6eb3bf
Alexander Kratzsch <alex@devrandom.se>
2015-06-25 14:21:56 +0200

    added forgotten relation

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -6,6 +6,7 @@ class Asset < ActiveRecord::Base
   diff include: [:serialized_components], exclude: [:created_at, :updated_at]
 
   has_many :statuses
+  has_many :reports
   has_many :revisions, as: :revisionable
   has_many :attached_components
   has_many :components, through: :attached_components
