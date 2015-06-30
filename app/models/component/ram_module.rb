9756f4550e0cb7047ddc24ba4443582254def349
Alexander Kratzsch <alex@devrandom.se>
2015-06-30 15:43:46 +0200

    seems like there is not always a rank to each ram module

--- app/models/component/ram_module.rb
+++ app/models/component/ram_module.rb
@@ -4,7 +4,7 @@ class Component::RamModule < Component
   validates :size_bytes, presence: true, numericality: {only_integer: true}
   validates :speed_mhz, presence: true, numericality: true
   validates :ecc, inclusion: [true, false]
-  validates :rank, presence: true
+  validates :rank, presence: true, allow_nil: true
   validates :ram_type, presence: true
   validates :form_factor, presence: true
 end
